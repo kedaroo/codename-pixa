05f57e1632f9e7f3500806d578d130b7dd246362
Siddhi Gate <siddhigate16@gmail.com>
2024-11-30 19:17:17 +0530

    feat: image slider (#8)
    * feat: image slider
    
    * lint
    
    * feat: photo gallery

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         >
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+              <nav className="sticky top-0 z-50 bg-background w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <HeaderAuth />
                 </div>
@@ -38,12 +38,6 @@ export default function RootLayout({
               <div className="flex flex-col gap-20 max-w-5xl p-5">
                 {children}
               </div>
-
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Footer
-                </p>
-              </footer>
             </div>
           </main>
         </ThemeProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import { Photo } from "@/components/types";
+import PhotoGallery from "@/components/ui/photo-gallery";
+import PhotoSlider from "@/components/ui/photo-slider";
 import { createClient } from "@/utils/supabase/server";
 
-export default async function Index() {
+const photo = {
+  src: "https://images.unsplash.com/photo-1731271140119-34ad9551ff10",
+  title: "Scenery",
+};
+const photos: Photo[] = new Array(10).fill("").map((_, idx) => ({
+  ...photo,
+  title: `${idx} ${photo.title}`,
+  id: Math.random().toString(),
+}));
 
+export default async function Index() {
   const supabase = await createClient();
 
   const {
@@ -9,12 +21,18 @@ export default async function Index() {
   } = await supabase.auth.getUser();
 
   if (!user) {
-    return <p>Welcome to our app!</p>
+    return <p>Welcome to our app!</p>;
   }
   return (
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
         Welcome {user.email}
+        <div>
+          <h2 className="my-4 font-bold text-3xl">Photo Slider</h2>
+          <PhotoSlider photos={photos} />
+          <h2 className="my-4 font-bold text-3xl">Photo Gallery</h2>
+          <PhotoGallery photos={photos} />
+        </div>
       </main>
     </>
   );
