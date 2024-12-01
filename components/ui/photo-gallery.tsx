378955ff78fa8035e0a178b3690fedbd0ab27679
Kedar Basutkar <krb1202@gmail.com>
2024-12-01 19:37:40 +0530

    make LG closeable

--- components/ui/photo-gallery.tsx
+++ components/ui/photo-gallery.tsx
@@ -18,10 +18,12 @@ const PhotoGallery = ({ photos }: Props) => {
 
   return (
     <div className="w-full flex justify-center">
-    <LightGallery 
-        elementClassNames=" grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 md:gap-3 w-full"
-        speed={500} 
-        plugins={[lgThumbnail, lgFullscreen]}
+    <LightGallery
+      closable
+      download={false}
+      elementClassNames="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 md:gap-3 w-full"
+      speed={500} 
+      plugins={[lgThumbnail, lgFullscreen]}
     >
       {photos.map((image, index) => (
         <a className="inline-block" key={index} href={image.src}>
