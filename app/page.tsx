74b766e4171fda15da4388aa01d943a540c74838
Kedar Basutkar <krb1202@gmail.com>
2024-12-01 17:51:27 +0530

    remove welcome msg

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,6 @@ export default async function Index() {
   return (
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
-        Welcome {user.email}
         <Sidepanel />
         <div>
           {/* <h2 className="my-4 font-bold text-3xl">Photo Slider</h2> */}
